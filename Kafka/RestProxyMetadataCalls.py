0c7ffc18e01d03cf4dcf78d8441a2feb621b08e3
Nishtha Bhattacharjee <npb258@myu.edu>
2020-10-09 16:31:40 -0400

    KafkaConnector and RestProxy

---
+++ Kafka/RestProxyMetadataCalls.py
@@ -0,0 +1,63 @@
+import json
+import requests
+
+
+REST_PROXY_URL = "http://localhost:8082"
+
+
+def get_topics():
+    """Gets topics from REST Proxy"""
+    # https://docs.confluent.io/current/kafka-rest/api.html#get--topics
+    resp = requests.get(f"{REST_PROXY_URL}/topics") 
+
+    try:
+        #Python request libary
+        resp.raise_for_status()
+    except:
+        print("Failed to get topics {json.dumps(resp.json(), indent=2)})")
+        return []
+
+    print("Fetched topics from Kafka:")
+    print(json.dumps(resp.json(), indent=2))
+    return resp.json()
+
+
+def get_topic(topic_name):
+    """Get specific details on a topic"""
+    # https://docs.confluent.io/current/kafka-rest/api.html#get--topics
+    resp = requests.get(f"{REST_PROXY_URL}/topics/{topic_name}")
+
+    try:
+        resp.raise_for_status()
+    except:
+        print("Failed to get topics {json.dumps(resp.json(), indent=2)})")
+
+    print("Fetched topics from Kafka:")
+    print(json.dumps(resp.json(), indent=2))
+
+
+def get_brokers():
+    """Gets broker information"""
+    # TODO See: https://docs.confluent.io/current/kafka-rest/api.html#get--brokers
+    resp = requests.get(f"{REST_PROXY_URL}/brokers") 
+
+    try:
+        resp.raise_for_status()
+    except:
+        print("Failed to get brokers {json.dumps(resp.json(), indent=2)})")
+
+    print("Fetched brokers from Kafka:")
+    print(json.dumps(resp.json(), indent=2))
+
+
+def get_partitions(topic_name):
+    """Prints partition information for a topic"""
+    #       See: https://docs.confluent.io/current/kafka-rest/api.html#get--topics-(string-topic_name)-partitions
+    print("get_partition is not yet implemented!")
+
+
+if __name__ == "__main__":
+    topics = get_topics()
+    get_topic(topics[0])
+    get_brokers()
+    get_partitions(topics[-1])
